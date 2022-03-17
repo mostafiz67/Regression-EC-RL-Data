144e2dcfdcdd405e2ea682853c729642ec492d37
mostafiz67 <mostafiz67.cu.cse@gmail.com>
2022-03-16 23:37:01 -0300

    added job scripts

--- run_brest_cancer.sh
+++ run_brest_cancer.sh
@@ -3,7 +3,7 @@
 #SBATCH --mem=60000M  # memory
 #SBATCH --cpus-per-task=32
 #SBATCH --output=Regression-EC-RL-Data-%j.out  # %N for node name, %j for jobID
-#SBATCH --time=00-00:20     # time (DD-HH:MM)
+#SBATCH --time=00-00:30     # time (DD-HH:MM)
 #SBATCH --mail-user=x2020fpt@stfx.ca # used to send emailS
 #SBATCH --mail-type=ALL
 
@@ -13,6 +13,6 @@ SOURCEDIR=/home/x2020fpt/projects/def-jlevman/x2020fpt/
 source /home/x2020fpt/projects/def-jlevman/x2020fpt/Regression-EC-RL-Data/.venv/bin/activate && echo "$(date +"%T"):  Activated python virtualenv"
 
 echo "$(date +"%T"):  start running model!"
-python3 /home/x2020fpt/projects/def-jlevman/x2020fpt/Regression-EC-RL-Data/run.py --dataset=House --progress
+python3 /home/x2020fpt/projects/def-jlevman/x2020fpt/Regression-EC-RL-Data/run.py --dataset=Breast-Cancer --progress
 echo "$(date +"%T"):  Finished running!"
 

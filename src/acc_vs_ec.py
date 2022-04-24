c29a477ae6f8f4a9963c15211c7562bfa5643fc1
mostafiz67 <mostafiz67.cu.cse@gmail.com>
2022-04-24 15:43:44 -0300

    added new EC method

--- src/acc_vs_ec.py
+++ src/acc_vs_ec.py
@@ -1,3 +1,4 @@
+# python3 -m src.acc_vs_ec
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
@@ -18,18 +19,20 @@ CSVS = [
 ]
 
 df = pd.concat([pd.read_csv(csv).assign(Dataset=csv.stem) for csv in CSVS]).reset_index(drop=True)
+df = df.drop(df[(df.Regressor == "SVR") | (df.Regressor == "Knn") | (df.Regressor == "RF") |
+                (df.Regressor == "Ridge") | (df.Regressor == "Lasso")].index) # removing non hypertune models data
 
 
 def ec_vs_accuracy():
     for method in df.Method.unique():
         data = df[df.Method.eq(method)]
         plt.figure(figsize=(10, 8))
-        sns.scatterplot(data=data, x="EC", y="R2", hue="Dataset", style="Regressor", s=100)
+        sns.scatterplot(data=data, x="EC", y="MSqE", hue="Dataset", style="Regressor", s=100)
         plt.legend(bbox_to_anchor=(1, 0.5), borderaxespad=0, loc="center left")
         plt.xlabel("Error Consistency (EC)")
-        plt.ylabel("R-Squared (R2)")
-        plt.title(f"Error Consistency ({method}) vs R2")
-        plt.savefig(PLOT_OUTPUT_PATH / 'new_medical_plots' / f"{method}_R2.png", bbox_inches='tight')
+        plt.ylabel("Mean Squared Error (MSqE)")
+        plt.title(f"Error Consistency ({method}) vs MSqE")
+        plt.savefig(PLOT_OUTPUT_PATH / 'new_medical_plots' / f"{method}_MSqE.png", bbox_inches='tight')
         plt.clf()
 
 

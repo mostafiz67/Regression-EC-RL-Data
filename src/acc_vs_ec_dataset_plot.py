af9ab1d9936025e8194cf759994152b9c0bade80
mostafiz67 <mostafiz67.cu.cse@gmail.com>
2022-05-07 22:43:27 -0300

    added plot code based on dataset

---
+++ src/acc_vs_ec_dataset_plot.py
@@ -0,0 +1,65 @@
+# python3 -m src.acc_vs_ec
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+from src.constants import OUT, PLOT_OUTPUT_PATH
+
+
+CSVS = [
+    OUT / "Wine_error.csv",
+    OUT / "Bike_error.csv",
+    # OUT / "House_error.csv",
+    # OUT / 'Breast-Cancer_error.csv',
+    # OUT / 'Cancer_error.csv',
+    # OUT / 'Diabetics_error.csv',
+    # OUT / 'Parkinsons-Tele_error.csv',
+]
+
+df = pd.concat([pd.read_csv(csv).assign(Dataset=csv.stem) for csv in CSVS]).reset_index(drop=True)
+df = df.drop(df[(df.Regressor == "SVR") | (df.Regressor == "Knn") | (df.Regressor == "RF") |
+                (df.Regressor == "Ridge") | (df.Regressor == "Lasso") | 
+                (df.Method == "negative_incon") | (df.Method == "positive_incon")].index) # removing non hypertune models data
+
+df['Dataset'].replace(regex=True,inplace=True,to_replace='_error',value=r'')
+df['Regressor'].replace(regex=True,inplace=True,to_replace='-H',value=r'')
+
+def ec_vs_accuracy_dataset():
+    for dataset in df.Dataset.unique():
+        print(df, dataset)
+        data = df[df["Dataset"] == dataset]
+        print(data)
+        plt.figure(figsize=(10, 8))
+        sns.scatterplot(data=data, x="EC", y="MAE", hue="Method", style="Regressor", s=100)
+        plt.legend(bbox_to_anchor=(1, 0.5), borderaxespad=0, loc="center left")
+        plt.xlabel("Error Consistency (EC)")
+        plt.ylabel("Mean Absolute Error (MAE)")
+        plt.title(f"Error Consistency vs MAE for the {dataset} dataset ")
+        plt.savefig(PLOT_OUTPUT_PATH / 'new_medical_plots' / f"MAE_{dataset}_only.png", bbox_inches='tight')
+        plt.clf()
+
+
+def ec_vs_accuracy_all_dataset():
+    # print(df, dataset)
+    data = df
+    print(data)
+    # plt.figure(figsize=(10, 8))
+    # sns.scatterplot(data=data, x="EC", y="MAE", hue="Method", style="Regressor", s=100)
+    main = sns.catplot(data=data, x="EC", y="MAE", hue="Dataset", col="Method", kind="point", col_wrap=2, height=10, aspect=3,
+    sharex=False, sharey=False)
+    # plt.legend(bbox_to_anchor=(1, 0.5), borderaxespad=0, loc="center left")
+    # axes = main.axes
+    # axes[0,0].set_ylim(0, )
+    # axes[0, 1].set_ylim(0, )
+    # main.set(ylim(0, None))
+    plt.xlabel("Error Consistency (EC)")
+    plt.ylabel("Mean Absolute Error (MAE)")
+    plt.title(f"Error Consistency vs MAE for alldataset ")
+    plt.savefig(PLOT_OUTPUT_PATH / 'new_medical_plots' / f"MAE_all_dataset.png", bbox_inches='tight')
+    plt.clf()
+
+
+
+if __name__ == "__main__":
+    # ec_vs_accuracy_dataset()
+    ec_vs_accuracy_all_dataset()
--- src/acc_vs_ec_plot.py
+++ src/acc_vs_ec_plot.py
@@ -2,8 +2,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-from pathlib import Path
-import os
 
 from src.constants import OUT, PLOT_OUTPUT_PATH
 
@@ -15,24 +13,26 @@ CSVS = [
     OUT / 'Breast-Cancer_error.csv',
     OUT / 'Cancer_error.csv',
     OUT / 'Diabetics_error.csv',
-    OUT / 'Parkinsons-Tele_error.csv',
+    # OUT / 'Parkinsons-Tele_error.csv',
 ]
 
 df = pd.concat([pd.read_csv(csv).assign(Dataset=csv.stem) for csv in CSVS]).reset_index(drop=True)
 df = df.drop(df[(df.Regressor == "SVR") | (df.Regressor == "Knn") | (df.Regressor == "RF") |
                 (df.Regressor == "Ridge") | (df.Regressor == "Lasso")].index) # removing non hypertune models data
 
+df['Dataset'].replace(regex=True,inplace=True,to_replace='_error',value=r'')
+df['Regressor'].replace(regex=True,inplace=True,to_replace='-H',value=r'')
 
 def ec_vs_accuracy():
     for method in df.Method.unique():
         data = df[df.Method.eq(method)]
         plt.figure(figsize=(10, 8))
-        sns.scatterplot(data=data, x="EC", y="MSqE", hue="Dataset", style="Regressor", s=100)
+        sns.scatterplot(data=data, x="EC", y="R2", hue="Dataset", style="Regressor", s=100)
         plt.legend(bbox_to_anchor=(1, 0.5), borderaxespad=0, loc="center left")
         plt.xlabel("Error Consistency (EC)")
-        plt.ylabel("Mean Squared Error (MSqE)")
-        plt.title(f"Error Consistency ({method}) vs MSqE")
-        plt.savefig(PLOT_OUTPUT_PATH / 'new_medical_plots' / f"{method}_MSqE.png", bbox_inches='tight')
+        plt.ylabel("R-Squared (R2)")
+        plt.title(f"Error Consistency ({method}) vs R2")
+        plt.savefig(PLOT_OUTPUT_PATH / 'new_medical_plots' / f"{method}_R2_House_only.png", bbox_inches='tight')
         plt.clf()
 
 
